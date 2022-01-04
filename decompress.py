23a85cb299cf96f7b02d929e389f6ab413dd38e5
Rohit Narayanan <rohitnanu.m@gmail.com>
2022-01-04 19:04:43 +0530

    Updated Decompression

--- decompress.py
+++ decompress.py
@@ -1,28 +1,37 @@
-def decompress(file):
-    text = open(file).read()
-    c=""
-    dct = [chr(a) for a in range(256)]
-    text=list(map(int,text.strip().split()))
-    prev=text[0]
-    out=dct[prev]
-    i=1
-    while i<len(text):
-        if text[i] >= len(dct):
-            s=dct[prev]
-            s=s+c
-        else:
-            s=dct[text[i]]
-        out+=s
-        c=s[0]
-        dct.append(dct[prev]+c)
-        prev=text[i]
-        i+=1
-    file=file[:file.rfind(".")]
-    file=file.replace("_compressed","")
-    with open(f"{file}_decompressed.txt","wb") as f:
-        f.write(out.encode())
-    return f"{file}_decompressed.txt"
+import os
+import sys
 
-# file=input()
-compress(input())
-decompress("output.txt")
+class decompress():
+    def __init__(self,file,out=None):
+        if not out:
+            out = os.path.splitdrive(file)[0]+".txt"
+        self.dict=[chr(a) for a in range(256)]
+        self.file=file
+        de=self.decompress(file)
+        with open(out,"w") as f:
+            f.write(de)
+
+
+    def decompress(text):
+        c = ""
+        text = list(map(int, text.strip().split(",")))
+        prev = text[0]
+        out = self.dict[prev]
+        i = 1
+        while i < len(text):
+            if text[i] >= len(dct):
+                s = self.dict[prev]
+                s = s+c
+            else:
+                s = self.dict[text[i]]
+            out += s
+            c = s[0]
+            dict.append(self.dict[prev]+c)
+            prev = text[i]
+            i += 1
+        return out
+
+if __name__=="__main__":
+    for i in sys.argv[1:]:
+        if os.path.isfile(i):
+            decompress(i)
