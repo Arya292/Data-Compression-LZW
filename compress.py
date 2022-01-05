8c4254f3cffe30fb7f7f1865d7107a6e3781b63c
Arya <aarryyaa292@gmail.com>
2022-01-05 14:37:15 +0530

    modified compress.py

--- compress.py
+++ compress.py
@@ -1,18 +1,21 @@
-def compress(text):
-    ascii_list= [chr(i) for i in range(128)]
-    output,a="",text[0]
+def compress(txt):
+    with open(txt) as f:
+        contents = f.read()
+    table= [chr(i) for i in range(256)]
+    output,a="",contents[0]
     i=1
-    while i<=(len(text)-1):
-        b=text[i]
-        if a+b in ascii_list:
+    while i<=(len(contents)-1):
+        b=contents[i]
+        if a+b in table:
             a=a+b
         else:
-            output=output+ str(ascii_list.index(a))+" "
-            ascii_list.append(a+b)
+            output=output+ str(table.index(a))+" "
+            table.append(a+b)
             a=b
         i=i+1
-    output=output+ str(ascii_list.index(a))+" "
-    return output
+    output=output+ str(table.index(a))+" "
+    new=open("Compressed.txt","w+")
+    new.write(output)
 
 file=input()
-print(compression(file))
+compress(file)
